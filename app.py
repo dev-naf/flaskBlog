f2a026da960c56f5d0f5fa8701bddd5b5523c272
dev-naf <ahnaf.fattah1@gmail.com>
2022-06-02 20:52:25 +0700

    add template

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 # Melakukan Import Flask, render Template
 from flask import Flask,render_template
+# Melakukan import FLASK-WTF dan Validator
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
 
 # Create Instiate Flask App / Inisialisasi Flask
-
 app =  Flask(__name__)
+# Membuat Secret Key, Mencegah Pemalsuan Form
+app.config['SECRET_KEY'] ='!!!'
+
+# Class Form, digunakan untuk keperluan pembuatan FORM dan integrasi
+# dengan Validator
+class makeForm(FlaskForm):
+    name = StringField("What's Your Name",validator=DataRequired)
+    submit = SubmitField('Submit')
 
 # membuat sebuah route
 # Membuat routes dengan page
@@ -16,8 +27,13 @@ def index():
 
 # Membuat route dengan Params
 @app.route('/dev/<name>')
-def name(name):
-    return '<h1>Selamat Malam {}, Kamu sudah berhasil membuat app. Flask'.format(name)
+def devPage(name):
+    return render_template('userProfile.html',data = name)
+
+# Membuat Route page Name, untuk implementasi form
+@app.route('/name',methods=['GET','POST'])
+def namepage():
+    return render_template('name.html')
 
 # membuat Custom Error Page
 # Invalid URL Error
@@ -43,9 +59,23 @@ def urlError(error):
 # 8a. Filter -> save, uppercase, lowercase, capitalize,title,trim, striptags [ok]
 # 9. Add Jinja statements, looping or if statements [ok]
 # 10. Add Custom Error Pages [OK]
-# 11. Membuat SSH KEY public maupun private []
-# 12. Inisialisasi Public SSH KEY -> GitHUB []
-# 13. Membuat File .gitignore untuk mengabaikan file yang akan di commit
-# 14. Membuat Repository dan melakukan PUSH []
+# 11. Membuat SSH KEY public maupun private [OK]
+# 12. Inisialisasi Public SSH KEY -> GitHUB [OK]
+# 13. Membuat File .gitignore untuk mengabaikan file yang akan di commit [OK]
+# 14. Membuat Repository dan melakukan PUSH [OK]
+# 15. Membuat Template, dengan Jinja [OK] = Kita Tidak perlu membuat navbar disetiap page
+# 16. Menggunakan Bootstrap pada Jinja [OK]
+# 17. Membuat Navigation BAR [OK]
+# 18. Modification NavBar [OK]
+# 19. Menggunakan Expression Url_for pada href [OK]
+# 20. Menggunakan Expression URL_FOR pada href, dengan parameter [OK]
+# 21. Donwload Library flask-wtf [OK]
+# 22. import flask-wtf dan wtforms,witform-validators [ok]
+# 23. Membuat Secret Key [ok]
+# 23. Membuat class form
+# 24. Membuat Variable Textfield dan SubmitField + validator
+# 25. Membuat Page 
+# 26. Membuat Routing untuk page tersebut
+# 27.
 
 #https://www.youtube.com/watch?v=4yaG-jFfePc&list=PLCC34OHNcOtolz2Vd9ZSeSXWc8Bq23yEz&index=2
